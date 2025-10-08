d829f2f91c0dace560ab8b0d61b665280513f3ae
João Júnior <122739421+joao-junior-dev@users.noreply.github.com>
2025-10-08 12:02:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Task
-# 1: Users can add an expense with a description and amount.
-# 2: Users can update an expense.
-# 3: Users can delete an expense.
-# 4: Users can view all expenses.
-# 5: Users can view a summary of all expenses.
-# 6: Users can view a summary of expenses for a specific month (of current year).
-# 7 : Add expense categories and allow users to filter expenses by category.
+# 1: Users can add an expense with a description and amount. ok
+# 2: Users can update an expense. ok
+# 3: Users can delete an expense.ok
+# 4: Users can view all expenses. ok
+# 5: Users can view a summary of all expenses. ok
+# 6: Users can view a summary of expenses for a specific month (of current year). ok
+# 7 : Add expense categories and allow users to filter expenses by category. ok
 # 8: Allow users to set a budget for each month and show a warning when the user exceeds the budget.
 # 9: Allow users to export expenses to a CSV file.
 from datetime import datetime
